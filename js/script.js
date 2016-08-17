4687cefd9e72fd3e2ce52352fca048f3a5ffdc7a
Brian Dutz and Stephen Newkirk <brian@>
2016-08-17 08:52:51 -0700

    add next step of behavior, return false for years divisible by 100

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@
 
 
 var leapYear = function(year){
-  if(year % 4 === 0){
+  if (year % 100 === 0){
+    return false;
+  }
+  else if(year % 4 === 0){
     return true;
   } else{
     return false;
