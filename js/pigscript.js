b0d2ccc5c697c15318464b02622fb1191a58cfb9
Brian Dutz and Stephen Newkirk <stephen@>
2016-08-17 11:33:43 -0700

    Create first three behaviors for translate function

--- js/pigscript.js
+++ js/pigscript.js
@@ -1,10 +1,19 @@
 // ==BUSINESS=LOGIC===
+var consonants = ["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var translate = function(string){
+  var lowerString = string.toLowerCase();
+  if (lowerString.charAt(0) === "a" || lowerString.charAt(0) ==="e" || lowerString.charAt(0) ==="i" || lowerString.charAt(0) ==="o" || lowerString.charAt(0) ==="u"){
+    return lowerString + "ay";
+  } else {
+    return lowerString;
+  }
+};
 
-
+// stringArray[0].includes(consonantArray);
 
 // ==UI=LOGIC==
 $(document).ready(function(){
-  $("form#sentence").submit(function(event){
+  $("form#pig-latin").submit(function(event){
     event.preventDefault();
     var sentenceInput = $("input#sentenceInput").val();
     var sentenceTranslated = translate(sentenceInput);
